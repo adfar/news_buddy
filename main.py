f26c9b8b57a13785dd2e79c5c43dcd3d82d97a5e
adfar <adf5092@gmail.com>
2024-02-28 01:29:46 -0500

    replace finnhub with LangChain/OpenAI. replace form default action with JS post.

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
-
-import finnhub
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import ChatOpenAI
+from typing import Annotated
 
 app = FastAPI()
 
+# for fetch api. change allowed origins before deploying
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
-finnhub_client = finnhub.Client(api_key="cdinfiiad3i9g9pvtmm0cdinfiiad3i9g9pvtmmg")
-current_price = finnhub_client.quote("AAPL")["c"]
+
+@app.post("/summary")
+async def summarize(url: Annotated[str, Form()]):
+    """Returns a summary of 'url'"""
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4-turbo-preview")
+    chain = load_summarize_chain(llm, chain_type="stuff")
+    output = chain.invoke(docs)
+    return output["output_text"]
 
 
-@app.get("/")
-async def index():
-    return {"price": current_price}
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
